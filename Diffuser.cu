ffee1cdb7161fe980fbe0d9cb7bd3890d045311b
Satya Arjunan <satya.arjunan@gmail.com>
2017-01-27 11:55:38 +0900

    check 20 neighbors: 13.3 BUPS

--- Diffuser.cu
+++ Diffuser.cu
@@ -85,8 +85,120 @@ double Diffuser::get_D() const {
   return D_;
 }
 
-//With offsets Titan X: 39 BUPS
-//With offsets: 18.6 BUPS
+//With offsets Titan X: 40 BUPS
+__global__
+void concurrent_walk(
+    const unsigned mol_size_,
+    const voxel_t stride_,
+    const voxel_t id_stride_,
+    const voxel_t vac_id_,
+    const voxel_t null_id_,
+    const umol_t num_voxels_,
+    umol_t* mols_) {
+  __shared__ int offsets_[48];
+  if(threadIdx.x == 0) {
+    //col=even, layer=even
+    offsets_[0] = -1;
+    offsets_[1] = 1;
+    offsets_[2] = -NUM_ROW-1;
+    offsets_[3] = -NUM_ROW;
+    offsets_[4] = NUM_ROW-1;
+    offsets_[5] = NUM_ROW;
+    offsets_[6] = -NUM_COLROW-NUM_ROW;
+    offsets_[7] = -NUM_COLROW-1;
+    offsets_[8] = -NUM_COLROW;
+    offsets_[9] = NUM_COLROW-NUM_ROW;
+    offsets_[10] = NUM_COLROW-1;
+    offsets_[11] = NUM_COLROW;
+
+    //col=even, layer=odd +24 = %layer*24
+    offsets_[24] = -1;
+    offsets_[25] = 1;
+    offsets_[26] = -NUM_ROW;
+    offsets_[27] = -NUM_ROW+1;
+    offsets_[28] = NUM_ROW;
+    offsets_[29] = NUM_ROW+1;
+    offsets_[30] = -NUM_COLROW;
+    offsets_[31] = -NUM_COLROW+1;
+    offsets_[32] = -NUM_COLROW+NUM_ROW;
+    offsets_[33] = NUM_COLROW;
+    offsets_[34] = NUM_COLROW+1;
+    offsets_[35] = NUM_COLROW+NUM_ROW;
+
+    //col=odd, layer=even +12 = %col*12
+    offsets_[12] = -1;
+    offsets_[13] = 1;
+    offsets_[14] = -NUM_ROW;
+    offsets_[15] = -NUM_ROW+1;
+    offsets_[16] = NUM_ROW;
+    offsets_[17] = NUM_ROW+1;
+    offsets_[18] = -NUM_COLROW-NUM_ROW;
+    offsets_[19] = -NUM_COLROW;
+    offsets_[20] = -NUM_COLROW+1;
+    offsets_[21] = NUM_COLROW-NUM_ROW;
+    offsets_[22] = NUM_COLROW;
+    offsets_[23] = NUM_COLROW+1;
+
+    //col=odd, layer=odd +36 = %col*12 + %layer*24
+    offsets_[36] = -1;
+    offsets_[37] = 1;
+    offsets_[38] = -NUM_ROW-1;
+    offsets_[39] = -NUM_ROW;
+    offsets_[40] = NUM_ROW-1;
+    offsets_[41] = NUM_ROW;
+    offsets_[42] = -NUM_COLROW-1;
+    offsets_[43] = -NUM_COLROW; //a
+    offsets_[44] = -NUM_COLROW+NUM_ROW;
+    offsets_[45] = NUM_COLROW-1;
+    offsets_[46] = NUM_COLROW;
+    offsets_[47] = NUM_COLROW+NUM_ROW;
+  }
+  __shared__ unsigned vdx[512];
+  __shared__ unsigned tars[512];
+  __syncthreads();
+  //index is the unique global thread id (size: total_threads)
+  unsigned index(blockIdx.x*blockDim.x + threadIdx.x);
+  const unsigned total_threads(blockDim.x*gridDim.x);
+  curandState local_state = curand_states[blockIdx.x][threadIdx.x];
+  while(index < mol_size_) {
+    const uint32_t rand32(curand(&local_state));
+    uint16_t rand16((uint16_t)(rand32 & 0x0000FFFFuL));
+    uint32_t rand(((uint32_t)rand16*12) >> 16);
+    vdx[threadIdx.x] = mols_[index];
+    bool odd_lay((vdx[threadIdx.x]/NUM_COLROW)&1);
+    bool odd_col((vdx[threadIdx.x]%NUM_COLROW/NUM_ROW)&1);
+    tars[threadIdx.x] = mol2_t(vdx[threadIdx.x])+
+      offsets_[rand+(24&(-odd_lay))+(12&(-odd_col))];
+    __syncthreads();
+    for(unsigned i(0); i != 20; ++i) {
+      if(tars[i] == tars[threadIdx.x]) {
+        tars[i] = vdx[i];
+      }
+      __syncthreads();
+    }
+    mols_[index] = tars[threadIdx.x];
+    index += total_threads;
+    __syncthreads();
+  }
+  curand_states[blockIdx.x][threadIdx.x] = local_state;
+}
+
+void Diffuser::walk() {
+  const size_t size(mols_.size());
+  concurrent_walk<<<blocks_, 512>>>(
+      size,
+      stride_,
+      id_stride_,
+      vac_id_,
+      null_id_,
+      num_voxels_,
+      thrust::raw_pointer_cast(&mols_[0]));
+  cudaDeviceSynchronize();
+
+}
+
+/*
+//With offsets Titan X: 40 BUPS
 __global__
 void concurrent_walk(
     const unsigned mol_size_,
@@ -188,6 +300,7 @@ void Diffuser::walk() {
   cudaDeviceSynchronize();
 
 }
+*/
 
 /*
 //concurrent_walk: max = 168 GB/s, average 163 GB/s
